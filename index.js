1979b50cd229a7c4bf6bfd62b2fc324b97d2b6e5
vivianachoque <vivianaachoque@gmail.com>
2020-02-10 16:05:06 -0300

    mejore el codigo

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 /*Ejercicio 3 Hacer una función que permita saber cuántos días faltan para que acabe el año. */
 
-toYear = 2020;
-toMonth = 12;
-toDay = 31;
-
-
 function daysToFinishYear(){
-    today = 31;
+    date = new Date();
+    quantDays=31;
     let day = date.getDate();
     let year = 366;
     year - quantDays;
-
 }
 
+console.log (year-day);
+}
